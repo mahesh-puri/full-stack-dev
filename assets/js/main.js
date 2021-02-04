e40703911bc092b7c9b9a4bcd1fee49997f3c46e
mahesh-puri <mpuri146@gmail.com>
2021-02-04 16:20:36 +0530

    added PWA support

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("service worker registered"))
+    .catch((err) => console.log("service worker not registered", err));
+}
+
 /*===== MENU SHOW Y HIDDEN =====*/
 const navMenu = document.getElementById("nav-menu"),
   toggleMenu = document.getElementById("nav-toggle"),
---
+++ sw.js
@@ -0,0 +1,40 @@
+const staticCacheName = "site-static-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/assets/js/main.js",
+  "/assets/css/styles.css",
+  "/assets/img/perfil.jpg",
+  "/assets/img/about.jpg",
+  "/assets/img/projectmind.jpg",
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap",
+];
+// install event
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
