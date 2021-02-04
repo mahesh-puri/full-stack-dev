bda027ac50f7e04afdb322efe748a5d9d0fe0c05
mahesh-puri <mpuri146@gmail.com>
2021-02-04 17:46:02 +0530

    added PWA support

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const staticCacheName = "site-static-v1";
 const assets = [
   "/",
-  "/index.html",
-  "/assets/js/main.js",
-  "/assets/css/styles.css",
-  "/assets/img/perfil.jpg",
-  "/assets/img/about.jpg",
-  "/assets/img/projectmind.jpg",
+  "./index.html",
+  "./assets/js/main.js",
+  "./assets/css/styles.css",
+  "./assets/img/perfil.jpg",
+  "./assets/img/about.jpg",
+  "./assets/img/projectmind.jpg",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap",
 ];
 // install event
