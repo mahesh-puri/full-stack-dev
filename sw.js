7d1b3589d19dcd04d4a45a2af95442bdecc45d3c
mahesh-puri <mpuri146@gmail.com>
2021-02-04 19:09:17 +0530

    added PWA support

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const assets = [
   "https://mahesh-puri.github.io/resume/assets/js/main.js",
   "https://mahesh-puri.github.io/resume/assets/css/styles.css",
   "https://mahesh-puri.github.io/resume/assets/img/perfil.jpg",
+  "https://mahesh-puri.github.io/resume/assets/img/logo/maskable_icon_x1.png",
   "https://mahesh-puri.github.io/resume/assets/img/about.jpg",
   "https://mahesh-puri.github.io/resume/assets/img/projectmind.jpg",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap",
