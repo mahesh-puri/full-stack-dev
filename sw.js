4b0806b7bda1a3d37af3f477a57a0bdfcdd43da8
mahesh-puri <mpuri146@gmail.com>
2021-02-11 23:41:22 +0530

    added PWA support

--- sw.js
+++ sw.js
@@ -32,6 +32,7 @@ self.addEventListener("activate", (evt) => {
 });
 // fetch event
 self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => {
       return cacheRes || fetch(evt.request);
